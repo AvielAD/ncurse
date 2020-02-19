8254c4c256924318626fe0dc19eb4652692a8f41
aviel aldama <aviel.aldama@gmail.com>
2020-02-19 00:12:47 -0600

    commit initial

---
+++ colors.c
@@ -0,0 +1,19 @@
+#include <curses.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	initscr();
+	start_color();
+	init_pair(1,COLOR_BLUE,COLOR_YELLOW);
+	init_pair(2,COLOR_RED,COLOR_YELLOW);
+	bkgd(COLOR_PAIR(2));
+	attron(COLOR_PAIR(1) | A_UNDERLINE);
+	move(10,40);
+	attron(A_REVERSE);
+	printw("HOLA, MUNDO");
+	getch();
+	endwin();
+	exit(0);
+}
